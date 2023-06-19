022e9e7b05714ac273034fde72ce49feb9e73a33
Relu Dumitru <dum.relu@gmail.com>
2023-06-19 15:54:54 +0300

    Initial commit

---
+++ const.py
@@ -0,0 +1,7 @@
+"""Constants for the Bucharest Hot Water Sensor integration."""
+from datetime import timedelta
+
+DOMAIN = "ha_bucharest_hot_water_sensor"
+
+# TODO: proper interval here
+SCAN_INTERVAL = timedelta(seconds=10)
